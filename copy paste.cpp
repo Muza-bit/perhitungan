3004af3636c6fa317ecf45e2fd8cf2e3c72d9971
Muza-bit <samsunew037@gmail.com>
2024-09-29 18:56:43 +0700

    Update copy paste.cpp

--- copy paste.cpp
+++ copy paste.cpp
@@ -2,18 +2,14 @@
 using namespace std;
 
 int main() {
-    // Deklarasi variabel
     float hargaAwal = 150000;
     float diskonPersen = 12.5;
     float besarnyaDiskon, hargaBersih;
 
-    // Menghitung besarnya diskon
-    besarnyaDiskon = (diskonPersen / 100) * hargaAwal;
+     besarnyaDiskon = (diskonPersen / 100) * hargaAwal;
 
-    // Menghitung harga bersih yang harus dibayar
-    hargaBersih = hargaAwal - besarnyaDiskon;
+     hargaBersih = hargaAwal - besarnyaDiskon;
 
-    // Menampilkan hasil
     printf("Harga awal: Rp %.2f\n", hargaAwal);
     printf("Besarnya diskon: Rp %.2f\n", besarnyaDiskon);
     printf("Harga bersih: Rp %.2f\n", hargaBersih);
